13262fdb02c84985195b66f3d6308f84db8b4aba
Prathamesh Badve <padve.96@gmail.com>
2025-08-16 22:15:27 +0530

    removed the run_sql function as it was no longer required

--- backend/funcs.py
+++ backend/funcs.py
@@ -1,31 +1,5 @@
-import sqlite3
 import pandas as pd
 import re
-
-def run_sql(db='test_db.db'):
-
-    # Connect to the database
-    with sqlite3.connect(db) as conn:
-
-        # SQL query to join Customers and Sales
-        query = """
-        SELECT 
-            s.sale_id,
-            c.name AS customer_name,
-            c.email,
-            s.product,
-            s.quantity,
-            s.sale_date
-        FROM Sales s
-        JOIN Customers c ON s.customer_id = c.customer_id
-        LIMIT 10
-        """
-
-        # Read query directly into a pandas DataFrame
-        df = pd.read_sql_query(query, conn)
-
-    return df
-
 
 def extract_sql(response_text):
     
